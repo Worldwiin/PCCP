7e6fdada1c5f6606cd4f69961c6772b0218d81e5
Vishvajeet <165566856+Worldwiin@users.noreply.github.com>
2024-05-23 21:49:13 +0530

    Add files via upload

---
+++ src/Database/DatabaseConnection.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author CCFD
+ */
+public class DatabaseConnection {
+
+    String db = "pccp";
+    String username = "root";
+    String password = "root";
+    Connection con = null;
+    Statement stmt;
+    ResultSet rs = null;
+
+    public DatabaseConnection() {
+    }
+
+    public Connection dbconnection() {
+
+        try {
+            String url = "jdbc:mysql://localhost:3306/" + db;
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+            if (con == null || con.isClosed()) {
+                con = DriverManager.getConnection(url, username, password);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+
+    public ResultSet getResultSet(String query) {
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+
+    public int getUpdate(String query) {
+        int i = 0;
+        try {
+            stmt = con.createStatement();
+            i = stmt.executeUpdate(query);
+        } catch (Exception e) {
+        }
+        return i;
+    }
+}
